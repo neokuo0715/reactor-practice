bcabaa915887d070530b4d5c4bced3e64c25b854
Neo Kuo <2012006@iisigroup.com>
2023-05-26 11:38:57 +0800

    initial commit

---
+++ src/test/java/com/example/lab/reactorLab/ReactorLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab.reactorLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactorLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
